61e7bda7633b90b5da8995f615f982864b7d87f3
Soulter <905617992@qq.com>
2024-07-31 20:22:17 +0800

    fix: buggy response after refreshing page

--- frontend/tick-stats-fe/src/stores/global.js
+++ frontend/tick-stats-fe/src/stores/global.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useGlobalStore = defineStore('global', {
   state: () => ({
-    baseUrl: window.location.href.replace(/\/$/, ''),
+    baseUrl: window.location.href.replace(window.location.pathname, ''),
     account: {
         name: '',
         email: '',
