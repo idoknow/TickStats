3e68dbab0eaca7db266b72f6e7134076b0b6f06b
Soulter <905617992@qq.com>
2024-10-23 17:47:41 +0800

    chore: update global.js

--- frontend/tick-stats-fe/src/stores/global.js
+++ frontend/tick-stats-fe/src/stores/global.js
@@ -1,19 +1,38 @@
 import { defineStore } from 'pinia'
+import { fetchWrapper } from '@/assets/utils'
 
 export const useGlobalStore = defineStore('global', {
   state: () => ({
     baseUrl: window.location.href.replace(new RegExp(window.location.pathname + '$'), ''),
     // baseUrl: 'http://localhost:8080',
     account: {
+        loaded: false,
         id: '',
         name: '',
         email: '',
+        account_apps: [],
     },
-    account_apps: [],
   }),
   actions: {
     updateState(payload) {
         this.$patch(payload)
+    },
+    async getAccount() {
+      if (this.account.loaded) {
+        return Promise.resolve(this.account)
+      } else {
+        const data = await fetchWrapper('/api/account/auth')
+        this.updateState({
+          account: {
+            loaded: true,
+            id: data.id,
+            name: data.name,
+            email: data.email,
+            account_apps: data.apps,
+          }
+        })
+        return this.account
+      }
     }
   }
 })
